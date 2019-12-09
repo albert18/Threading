6a13e24ee306a8f4065a7428bdbd890074639576
albert18 <caps18@hotmail.com>
2019-12-08 21:03:22 -0500

    update

--- Program.cs
+++ Program.cs
@@ -3,58 +3,88 @@ using System.Threading;
 
 namespace coreConsole
 {
-    //Thread
-
-
     class Program
     {
-        private static bool isCompleted;
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine(Thread.CurrentThread.IsThreadPoolThread);
 
-        static readonly object lockCompleted = new object();
+            Employee employee = new Employee();
+            employee.Name = "Albert";
+            employee.CompanyName = "SomeWhere";
 
-        static void Main(string[] args)
-        {   
-            #region 1
-            // Thread thread = new Thread(HelloWorld);
+            ThreadPool.QueueUserWorkItem(new WaitCallback(DisplayEmployee), employee);
 
-            // thread.Start();
+            var processorCount = Environment.ProcessorCount;
+            ThreadPool.SetMaxThreads(20,30);
+            int workerThreads = 0;
+            int completionPortThreads = 0;
+            ThreadPool.GetAvailableThreads(out workerThreads, out completionPortThreads);
 
-            // HelloWorld();                
+            System.Console.WriteLine(Thread.CurrentThread.IsThreadPoolThread);
 
-
-            // new Thread(PrintOneToThirty).Start();
-
-            // PrintOneToThirty();
-            #endregion
-
- 
-
+            
         }
 
+        private static void DisplayEmployee(object employee)
+        {
+            System.Console.WriteLine(Thread.CurrentThread.IsThreadPoolThread);
+            Employee emp = employee as Employee;
+            System.Console.WriteLine("Person Name is {0} and the company name {1}", emp.Name, emp.CompanyName);
+        } 
+    }
 
-
-
-        #region 1
-        // private static void HelloWorld()
-        // {
-        //     lock (lockCompleted)
-        //     {
-        //         if (!isCompleted)
-        //             {
-        //                 isCompleted = true;
-        //                 System.Console.WriteLine("Hello World!!");    
-        //             }
-        //     }
-        // }    
-
-        // private static void PrintOneToThirty()
-        // {
-        //     for (int i = 0; i < 30; i++)
-        //     {
-        //         System.Console.WriteLine(i + 1 + " ");
-        //     }
-        // }
-        #endregion
-
+    class Employee
+    {
+        public string Name { get; set; }
+        public string CompanyName { get; set; }
     }
 }
+
+
+
+
+
+
+// private static bool isCompleted;
+
+// static readonly object lockCompleted = new object();
+
+// static void Main(string[] args)
+// {   
+//     #region 1
+//     // Thread thread = new Thread(HelloWorld);
+
+//     // thread.Start();
+
+//     // HelloWorld();                
+
+
+//     // new Thread(PrintOneToThirty).Start();
+
+//     // PrintOneToThirty();
+//     #endregion
+
+// }
+
+// #region 1
+// // private static void HelloWorld()
+// // {
+// //     lock (lockCompleted)
+// //     {
+// //         if (!isCompleted)
+// //             {
+// //                 isCompleted = true;
+// //                 System.Console.WriteLine("Hello World!!");    
+// //             }
+// //     }
+// // }    
+
+// // private static void PrintOneToThirty()
+// // {
+// //     for (int i = 0; i < 30; i++)
+// //     {
+// //         System.Console.WriteLine(i + 1 + " ");
+// //     }
+// // }
+// #endregion
