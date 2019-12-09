84b4175b5c0a2b1066f481aeeed78092c6c8b813
albert18 <caps18@hotmail.com>
2019-12-08 19:21:00 -0500

    update Threading

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace coreConsole
+{
+    //Thread
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Thread thread = new Thread(WriteNewThread);
+               thread.Start();
+
+            //Main Thread
+            for (int i = 0; i < 1000; i++)
+            {
+                System.Console.WriteLine(" A" + i);
+            }
+
+            
+        }
+
+        private static void WriteNewThread()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                System.Console.WriteLine(" Z" + i);
+            }
+        }
+    }
+}
