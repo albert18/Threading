5a336a5eacf0c413df474fca908384f700f40d95
albert18 <caps18@hotmail.com>
2019-12-12 09:14:39 -0500

    Update

--- TASK/Program.cs
+++ TASK/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Threading;
 
 namespace TASK
 {
     class Program
     {
         static void Main(string[] args)
+        {   
+            Thread thread = new Thread(PrintHellowWorld);
+            thread.Start();
+            thread.Join();
+            System.Diagnostics.Debug.WriteLine("Hello World Printed");
+
+            // System.Console.WriteLine("Hello World Printed");
+
+
+            
+        }
+
+        private static void PrintHellowWorld()
         {
-            Console.WriteLine("Hello World!");
+            System.Diagnostics.Debug.WriteLine("Hello World");
+            // System.Console.WriteLine("Hello World");
+            Thread.Sleep(5000);
         }
     }
 }
+
+
+
+// Other Thread
+// static void Main(string[] args)
+// {   
+//     Thread thread = new Thread(PrintHellowWorld);
+//     thread.Start();
+//     thread.Join();
+//     System.Diagnostics.Debug.WriteLine("Hello World Printed");
+
+//     // System.Console.WriteLine("Hello World Printed");
+
+
+    
+// }
+
+// private static void PrintHellowWorld()
+// {
+//     System.Diagnostics.Debug.WriteLine("Hello World");
+//     // System.Console.WriteLine("Hello World");
+//     Thread.Sleep(5000);
+// }
+
